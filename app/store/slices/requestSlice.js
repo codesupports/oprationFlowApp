8ef961b5f84564f298afd2f02dc0de2a880e7ca3
codesupports <rajkumar.mah55@gmail.com>
2026-08-24 17:17:51 +0530

    Api path fix

--- app/store/slices/requestSlice.js
+++ app/store/slices/requestSlice.js
@@ -5,7 +5,7 @@ export const requestApi = createApi({
     reducerPath: "requestApi",
 
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:3000", // https://opration-flow-app.vercel.app/
+        baseUrl: " https://opration-flow-app.vercel.app/", // https://opration-flow-app.vercel.app/
     }),
 
     tagTypes: ["Requests"],
